325ca27c2e47198196eb2fb68dd3c4bf0b3a3c21
HansJack <2310210201@tiangong.edu.cn>
2026-01-11 19:53:58 +0800

    chore(release): 升级版本号至0.3.2
    - 更新update.json中的最新版本号从0.3.1到0.3.2
    - 添加0.3.2到版本列表中
    - 更新Android项目的versionCode从9到10
    - 更新Android项目的versionName从0.3.1到0.3.2

--- apps/zhixu-android/build.gradle.kts
+++ apps/zhixu-android/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "app.zhixu"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 9
-        versionName = "0.3.1"
+        versionCode = 10
+        versionName = "0.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
         externalNativeBuild {
