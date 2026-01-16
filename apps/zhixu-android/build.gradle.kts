25373ddea21e161827e48fb85a9d004c91977ff2
HansJack <2310210201@tiangong.edu.cn>
2026-01-16 21:22:56 +0800

    chore(release): 升级版本号至0.3.3

--- apps/zhixu-android/build.gradle.kts
+++ apps/zhixu-android/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "app.zhixu"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 10
-        versionName = "0.3.2"
+        versionCode = 11
+        versionName = "0.3.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
         externalNativeBuild {
